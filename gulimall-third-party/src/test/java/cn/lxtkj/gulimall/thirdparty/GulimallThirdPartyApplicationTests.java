89b93fe70e50a66a2ca58857c4943eab7f596c4d
leiwen <leiwen@heytea.com>
2021-09-09 17:14:34 +0800

    feat:添加第三方服务

---
+++ gulimall-third-party/src/test/java/cn/lxtkj/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,46 @@
+package cn.lxtkj.gulimall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GulimallThirdPartyApplicationTests {
+
+    @Resource
+    OSSClient ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+//        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI4tFnRMJQVvfsyXaurNmo";
+//        String accessKeySecret = "eKtByXS0L5lWiXrqLCzSbLfzgTBJlD";
+
+        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("E:\\demo\\test3.png");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("lxtkj", "test3.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功...");
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
