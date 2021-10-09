cbf1ed2b5f00fecce43e1a0e4f1e4fad21f884b0
leiwen <leiwen@heytea.com>
2021-10-09 15:05:44 +0800

    feat:解决session共享问题

---
+++ gulimall-auth/src/main/java/cn/lxtkj/gulimall/auth/config/SessionConfig.java
@@ -0,0 +1,38 @@
+package cn.lxtkj.gulimall.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @Description:
+ * @Created: By IntelliJ IDEA.
+ * @author: 雷小天
+ * @createTime: 2021/9/24 19:17
+ **/
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+
+        //放大作用域
+        cookieSerializer.setDomainName("gulimall.com");
+        cookieSerializer.setCookieName("GULISESSION");
+
+        return cookieSerializer;
+    }
+
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+}
